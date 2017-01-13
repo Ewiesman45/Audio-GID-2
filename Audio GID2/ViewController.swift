f4afb8bd85c74c94c37e34f5bf55de6e7de80c30
Evan Wiesman <EvanWiesman@Evans-MacBook-Air-2.local>
2017-01-13 10:38:24 -0600

    Initial Commit

---
+++ Audio GID2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Audio GID2
+//
+//  Created by Evan Wiesman on 1/13/17.
+//  Copyright © 2017 Evan Wiesman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Audio GID2Tests/Audio_GID2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Audio_GID2Tests.swift
+//  Audio GID2Tests
+//
+//  Created by Evan Wiesman on 1/13/17.
+//  Copyright © 2017 Evan Wiesman. All rights reserved.
+//
+
+import XCTest
+@testable import Audio_GID2
+
+class Audio_GID2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
